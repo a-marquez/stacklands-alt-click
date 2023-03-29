0cf62c5c01bf238cc4379852b4bd42afc1827ed7
lopidav <poluhovichvadim@gmail.com>
2023-03-29 03:12:06 +0300

    glue fix
    Glue unstuck bug fix,
    Shift+alt targeting root glue bug fix,
    Some standardization and optimization

--- Plugin.cs
+++ Plugin.cs
@@ -18,64 +18,74 @@ namespace StacklandsAltClick
         private void Awake()
         {
             L = Logger;
-            try {
+            try
+            {
                 Harmony.CreateAndPatchAll(typeof(Plugin));
-            } catch(Exception E) {
+            }
+            catch(Exception E)
+            {
                 L.LogError(E.Message);
             }
         }
 
         // Reference: https://github.com/benediktwerner/Stacklands-BugFixes-Mod/blob/master/Plugin.cs#L65
-        [HarmonyPrefix]
         [HarmonyPatch(typeof(GameCard), nameof(GameCard.StartDragging))]
-        private static void DontDetachContainersFromGlueAfterAltClicking(GameCard __instance, out GameCard[] __state)
+        [HarmonyPrefix]
+        private static void DontDetachContainersFromGlueAfterAltClicking(GameCard __instance, out GameCard __state)
         {
 
-            __state = new GameCard[0];
+            __state = null;
 
-            if (!InputController.instance.GetKey(Key.LeftAlt) && !InputController.instance.GetKey(Key.RightAlt)) {
+            if (!InputController.instance.GetKey(Key.LeftAlt) && !InputController.instance.GetKey(Key.RightAlt))
+            {
                 return;
             }
 
-            if (__instance.CardData is not Chest && __instance.CardData is not ResourceChest) {
+            if (!checkIfClickableChest(__instance.CardData))
+            {
                 return;
             }
-
-            if (__instance.Parent?.CardData is not HeavyFoundation) {
-                return;
-            }
-
-            __state = new GameCard[2] { __instance, __instance.Parent };
+            __state = __instance.Parent;
 
         }
 
         [HarmonyPatch(typeof(GameCard), nameof(GameCard.StartDragging))]
         [HarmonyPostfix]
-        public static void StartDragging(GameCard __instance, GameCard[] __state)
+        public static void StartDragging(GameCard __instance, GameCard __state)
         {
-            if (!InputController.instance.GetKey(Key.LeftAlt) && !InputController.instance.GetKey(Key.RightAlt)) {
+            if (!InputController.instance.GetKey(Key.LeftAlt) && !InputController.instance.GetKey(Key.RightAlt))
+            {
                 return;
             }
+            
+            GameCard clickTarget = __instance;
 
-            if (__instance.CardData is not Chest && __instance.CardData is not ResourceChest) {
-                return;
+            if (!checkIfClickableChest(clickTarget.CardData))
+            {
+                clickTarget = clickTarget.GetLeafCard();
+                if (!checkIfClickableChest(clickTarget.CardData))
+                {
+                    return;
+                }
             }
 
-            if (__instance.CardData is Chest chest && chest.CoinCount <= 0) {
-                return;
-            }
 
-            if (__instance.CardData is ResourceChest resourceChest && resourceChest.ResourceCount <= 0) {
-                return;
-            }
+            int previousCardCount = WorldManager.instance.AllCards.Count;
+            clickTarget.Clicked();
+            GameCard newCard;
 
-            // Try to fix containers detaching from glue
-            if (__state != null && __state.Length > 0) {
-                L.LogInfo(__state);
+            if (previousCardCount >= WorldManager.instance.AllCards.Count)
+            {
+                newCard = __instance;
             }
-
-            __instance.Clicked();
-            GameCard newCard = WorldManager.instance.AllCards.Last().GetRootCard();
+            else
+            {
+                newCard = WorldManager.instance.AllCards.Last().GetRootCard();
+                if (__state)
+                {
+                    __instance.SetParent(__state);
+                }
+            }
 
             newCard.DragTag = null;
 
@@ -83,6 +93,11 @@ namespace StacklandsAltClick
             WorldManager.instance.DraggingDraggable.DragStartPosition = WorldManager.instance.DraggingDraggable.transform.position;
             WorldManager.instance.grabOffset = WorldManager.instance.mouseWorldPosition - WorldManager.instance.DraggingDraggable.transform.position;
             WorldManager.instance.DraggingDraggable.StartDragging();
+        }
+        public static bool checkIfClickableChest(CardData chestCandidate)
+        {
+            return (chestCandidate is Chest chest && chest.CoinCount > 0)
+                || (chestCandidate is ResourceChest resourceChest && resourceChest.ResourceCount > 0);
         }
 
     }
